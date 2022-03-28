e014484491ebd8d56fef5f1d57c77daf89748391
jacksonmalloy <jacksmalloy@gmail.com>
2022-03-28 07:23:12 -0700

    adds the ability to add / remove muscle groups from exercises. Also adds initial admin implementation.

---
+++ server/src/queries/removeMuscleGroupFromExercise.ts
@@ -0,0 +1,36 @@
+import { db } from "config/db";
+import { Response } from "express";
+
+export const removeMuscleGroupFromExercise = async (
+  res: Response,
+  exerciseId: string,
+  muscleGroupId: string,
+  group: "primary" | "secondary"
+) => {
+  const query = `DELETE FROM exercise_muscle_groups WHERE exercise_id = $1 AND muscle_group_id = $2 AND "group" = $3 RETURNING *;`;
+  const params = [exerciseId, muscleGroupId, group];
+  try {
+    const data = await db.query(query, params);
+
+    if (!data.rowCount) {
+      return res.status(404).json({
+        status: "error",
+        message: "Muscle-group does not exist",
+        muscleGroup: null,
+      });
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: "Muscle-group removed successfully",
+      muscleGroup: data.rows[0],
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({
+      status: "error",
+      message: "Database error",
+      muscleGroup: null,
+    });
+  }
+};
---
+++ server/src/routes/exercises/muscle-groups/index.ts
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import { authenticate } from "middlewares/authenticate";
+import { authorize } from "middlewares/authorize";
+import { addMuscleGroupToExercise } from "queries/addMuscleGroupToExercise";
+import { removeMuscleGroupFromExercise } from "queries/removeMuscleGroupFromExercise";
+
+const router = Router();
+
+// Add muscle-group to the exercise
+router.post(
+  "/:exerciseId/muscle-group/",
+  authenticate,
+  authorize,
+  async (req, res) => {
+    await addMuscleGroupToExercise(res, req.body, req.params.exerciseId);
+  }
+);
+
+// Remove primary muscle-group from the exercise
+router.delete(
+  "/:exerciseId/muscle-group/:muscleGroupId/primary",
+  authenticate,
+  authorize,
+  async (req, res) => {
+    await removeMuscleGroupFromExercise(
+      res,
+      req.params.exerciseId,
+      req.params.muscleGroupId,
+      "primary"
+    );
+  }
+);
+
+// Remove secondary muscle-group from the exercise
+router.delete(
+  "/:exerciseId/muscle-group/:muscleGroupId/secondary",
+  authenticate,
+  authorize,
+  async (req, res) => {
+    await removeMuscleGroupFromExercise(
+      res,
+      req.params.exerciseId,
+      req.params.muscleGroupId,
+      "secondary"
+    );
+  }
+);
+
+export default (parentRouter: Router) => {
+  parentRouter.use("/", router);
+};
