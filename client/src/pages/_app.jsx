b042d44191bb68967d0b51fb4622890dd6fa5b84
jacksonmalloy <jacksmalloy@gmail.com>
2022-03-28 20:48:08 -0700

    adds react-query simple demo to fetching data

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,10 +1,17 @@
 import Layout from "../features/common/Layout";
 import "../styles/globals.css";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,13 @@
+import { MuscleGroups } from "features/muscle-groups/MuscleGroups";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <MuscleGroups />
+    </div>
+  );
 };
 
 export default HomePage;
