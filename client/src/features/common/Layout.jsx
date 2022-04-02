8d6e62b45ca56d1b6401ea64c1f7ff9378ca25ca
jacksonmalloy <jacksmalloy@gmail.com>
2022-04-01 19:37:05 -0700

    adds a header to the layout, with links to profile & search (can change these later)

--- client/src/features/common/Layout.jsx
+++ client/src/features/common/Layout.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Footer from './Footer';
 import Navbar from './Navbar';
+import { Header } from './Header';
 
 const Layout = ({ children }) => {
   return (
     <main>
+      {/* Empty div to cover the height of the navbar */}
+      <div style={{ height: '40px' }} />
+      <Header />
+
       <section className="flex w-100 items-center justify-center">
         <div className="max-w-xl">{children}</div>
       </section>
+
+      {/* Empty div to cover the height of the navbar */}
+      <div style={{ height: '70px' }} />
       <Navbar />
     </main>
   );
