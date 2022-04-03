8476764943e637d8f7407f50ffacefe21c942da7
jacksonmalloy <jacksmalloy@gmail.com>
2022-04-03 15:17:43 -0700

    adding more variations of art SVGs to display on empty days

--- client/src/features/feed/Feed.jsx
+++ client/src/features/feed/Feed.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Divider } from 'features/feed/Divider';
 import { Panel } from 'features/feed/Panel';
+import { getFutureDate, isToday } from './utils';
 
 export const Feed = ({ workouts }) => {
   return (
-    <div>
+    <div className="pb-10">
       {Array.of(0, 1, 2, 3, 4, 5, 6).map((day) => {
         if (day === 0) {
           return (
-            <>
+            <React.Fragment key={day}>
               <Divider label="Today's Workout" />
               <Panel
+                day={day}
                 workouts={workouts.filter((workout) => {
                   if (!workout.workout_dt) {
                     return false;
@@ -18,15 +20,16 @@ export const Feed = ({ workouts }) => {
                   return isToday(new Date(workout.workout_dt), day);
                 })}
               />
-            </>
+            </React.Fragment>
           );
         }
 
         if (day === 1) {
           return (
-            <>
+            <React.Fragment key={day}>
               <Divider label="Tomorrows's Workout" />
               <Panel
+                day={day}
                 workouts={workouts.filter((workout) => {
                   if (!workout.workout_dt) {
                     return false;
@@ -34,18 +37,19 @@ export const Feed = ({ workouts }) => {
                   return isToday(new Date(workout.workout_dt), day);
                 })}
               />
-            </>
+            </React.Fragment>
           );
         }
 
         return (
-          <>
+          <React.Fragment key={day}>
             <Divider
               label={`${new Intl.DateTimeFormat('en-CA', {
                 weekday: 'long',
               }).format(getFutureDate(day))}'s Workout`}
             />
             <Panel
+              day={day}
               workouts={workouts.filter((workout) => {
                 if (!workout.workout_dt) {
                   return false;
@@ -53,25 +57,9 @@ export const Feed = ({ workouts }) => {
                 return isToday(new Date(workout.workout_dt), day);
               })}
             />
-          </>
+          </React.Fragment>
         );
       })}
     </div>
   );
-};
-
-const isToday = (someDate, index) => {
-  const today = getFutureDate(index);
-  return (
-    someDate.getDate() == today.getDate() &&
-    someDate.getMonth() == today.getMonth() &&
-    someDate.getFullYear() == today.getFullYear()
-  );
-};
-
-const getFutureDate = (index) => {
-  const today = new Date();
-  const futureDate = new Date(today);
-  futureDate.setDate(futureDate.getDate() + index);
-  return futureDate;
 };
--- client/src/features/feed/Panel.jsx
+++ client/src/features/feed/Panel.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Sleep } from 'features/art/Sleep';
 import { IndoorBike } from 'features/art/IndoorBike';
 import { Button } from 'features/common/Button';
+import { isToday } from './utils';
+import { HealthyHabit } from 'features/art/HealthyHabit';
+import { PersonalTrainer } from 'features/art/PersonalTrainer';
+import { PersonalTraining } from 'features/art/PersonalTraining';
+import { WorkingOut } from 'features/art/WorkingOut';
+import { FitnessTracker } from 'features/art/FitnessTracker';
+import { FitnessStats } from 'features/art/FitnessStats';
+import Link from 'next/link';
 
-export const Panel = ({ workouts }) => {
+export const Panel = ({ workouts, day }) => {
   if (!workouts.length) {
-    return <ScheduleNextDay />;
+    return <ScheduleNextDay day={day} />;
   }
 
   return <ScheduledWorkouts workouts={workouts} />;
@@ -27,20 +35,42 @@ const RestDay = () => {
   );
 };
 
-const ScheduleNextDay = () => {
+const ScheduleNextDay = ({ day }) => {
+  const ArtComponent = () => {
+    switch (day) {
+      case 0:
+        return <HealthyHabit />;
+      case 1:
+        return <IndoorBike />;
+      case 2:
+        return <PersonalTrainer />;
+      case 3:
+        return <FitnessStats />;
+      case 4:
+        return <PersonalTraining />;
+      case 5:
+        return <WorkingOut />;
+      case 6:
+        return <FitnessTracker />;
+      default:
+        return <FitnessStats />;
+    }
+  };
+
   return (
     <div className="w-80 my-4 px-2">
       <div className="flex flex-row items-start gap-4 px-2">
         <div className="w-full flex flex-row justify-between">
-          <div className="pt-2 w-40">
+          <div className="pt-2 w-80">
             <p className="text-xl font-medium">Schedule your next workout</p>
-            <button className="bg-white mt-2 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32">
-              Schedule
-            </button>
-            {/* <Schedule /> */}
+            <Link href="/workouts">
+              <button className="bg-white mt-2 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32">
+                Schedule
+              </button>
+            </Link>
           </div>
 
-          <IndoorBike />
+          <ArtComponent />
         </div>
       </div>
     </div>
@@ -67,7 +97,7 @@ const ScheduledWorkouts = ({ workouts }) => {
       <div className="w-80 mt-2 px-2 py-4" key={w.workout_id}>
         <div className="flex flex-row items-start gap-4">
           <div className="w-full flex flex-row justify-between">
-            <div className="pt-2">
+            <div className="pt-2 w-52">
               <p className="text-xl font-medium">{w.name}</p>
               <p className="text-xs">{w.description}</p>
             </div>
@@ -81,15 +111,31 @@ const ScheduledWorkouts = ({ workouts }) => {
             </aside>
           </div>
         </div>
-        <div className="flex items-center justify-between gap-4 mt-2">
-          <Button
-            type="button"
-            className="w-full"
-            onClick={() => startWorkout(w.workout_id)}
-          >
-            Start
-          </Button>
-        </div>
+
+        {isToday(new Date(w.workout_dt)) ? (
+          <div className="flex items-center justify-between gap-4 mt-2">
+            <Button
+              type="button"
+              className="w-full"
+              onClick={() => startWorkout(w.workout_id)}
+            >
+              Start
+            </Button>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between gap-4 mt-2">
+            <Button
+              type="button"
+              className="w-full disabled:border-none disabled:opacity-30"
+              isDisabled
+            >
+              Scheduled for{' '}
+              {new Intl.DateTimeFormat('en-CA', {
+                weekday: 'short',
+              }).format(new Date(w.workout_dt))}
+            </Button>
+          </div>
+        )}
       </div>
     );
   });
