75f143d99b974a388569f1ddbf7391839cc4376e
jacksonmalloy <jacksmalloy@gmail.com>
2022-04-09 09:19:22 -0700

    force create

--- client/src/pages/logbook/index.jsx
+++ client/src/pages/logbook/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Logbook } from 'features/Logbook';
 import Layout from 'features/common/Layout';
 
+// not using this page?
 const LogbookPage = () => {
   return (
     <Layout>
