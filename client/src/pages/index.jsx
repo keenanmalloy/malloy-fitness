cd656e68f9efc9b0b3ed2c3e46db15997a8234ad
keenanmalloy <malloyrkeenan@gmail.com>
2022-04-19 08:25:24 -0700

    homepage

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -48,21 +48,27 @@ function HomePage() {
       <div className="border-b border-slate-400">
         <div className="h-32 flex justify-center pt-4"></div>
         <div className="flex justify-center border-b-2 border-solid rounded-sm border-slate-400 mx-3 pb-4">
-          <div className="flex flex-col text-center text-slate-600">Cardio</div>
+          <div className="flex flex-col text-center text-slate-600">
+            No cardio tracked for today
+          </div>
         </div>
       </div>
 
       <div className="border-b border-slate-400">
         <div className="h-16 flex justify-center pt-4"></div>
         <div className="flex justify-center border-b-2 border-solid rounded-sm border-slate-400 mx-3 pb-4">
-          <div className="flex flex-col text-center text-slate-600">Sleep</div>
+          <div className="flex flex-col text-center text-slate-600">
+            No Sleep tracked for today
+          </div>
         </div>
       </div>
 
       <div className="border-b border-slate-400">
         <div className="h-16 flex justify-center pt-4"></div>
         <div className="flex justify-center border-b-2 border-solid rounded-sm border-slate-400 mx-3 pb-4">
-          <div className="flex flex-col text-center text-slate-600">Steps</div>
+          <div className="flex flex-col text-center text-slate-600">
+            No steps tracked for today
+          </div>
         </div>
       </div>
     </Layout>
