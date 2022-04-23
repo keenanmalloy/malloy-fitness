9b5711f6ff1c9efbfdcb1b5b1476a7326d5949a5
jacksonmalloy <jacksmalloy@gmail.com>
2022-04-23 12:59:13 -0700

    hotfix db connection

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,6 +4,11 @@ export const db = new Pool({
   max: 20,
   connectionString: process.env.DATABASE_URL,
   application_name: 'malloy-fitness',
-  ssl: false,
+  ssl:
+    process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
+      ? false
+      : {
+          rejectUnauthorized: false,
+        },
   connectionTimeoutMillis: 10000,
 });
