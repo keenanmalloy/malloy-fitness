d6489885cf8ea6fe4c833acb74fef333412a75e0
keenanmalloy <malloyrkeenan@gmail.com>
2022-05-03 23:28:17 -0700

    end workout page

--- client/src/features/daily/SleepPanel.jsx
+++ client/src/features/daily/SleepPanel.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { AuthorizeGoogleFitButton } from './AuthorizeGoogleFitButton';
 
 export const SleepPanel = ({ data }) => {
   return (
     <div>
-      <div className="text-center text-2xl text-slate-600">Sleep</div>
-      <div className="flex justify-center text-center border-b-2 border-solid rounded-sm border-slate-400 mx-3 pb-2">
+      <div className="flex justify-center text-center mx-3 py-2">
         7/10 hours (Daily/Weekly goal set in settings) Sleep Today
       </div>
     </div>
